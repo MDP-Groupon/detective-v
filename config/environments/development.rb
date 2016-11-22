9b38abe5b7f97b7be0fef2461b4d71207cdb898f
aegrobbel <aegrobbel@gmail.com>
2016-11-22 13:08:16 -0500

    scan button

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ,
-   :password             => ,
+   :user_name            => "detectivev2016@gmail.com",
+   :password             => "Hi123456",
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
